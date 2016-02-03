242d50bb4584fe689bee7fafa5f4d0e86d9274e6
Dafrok <88609546@qq.com>
2016-02-03 15:32:07 +0800

    Support mud

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-function PublicInterval (option) {
-    option = option || {}
-    this.timer = option.timer || 16.7
-    this.processor = option.processor || {}
-    this.flag = typeof option.autorun === 'bool' ? option.autorun : false
-    this.isRunning = false
-    this.handler = function () {
-        if (this.flag) {
-            for (key in this.processor) {
-                setTimeout(this.processor[key], 0)
+(function (global, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        define('PublicInterval', factory);
+    } else {
+        global.PublicInterval = factory();
+    }
+})(this, function () {
+    function PublicInterval (option) {
+        option = option || {}
+        this.timer = option.timer || 16.7
+        this.processor = option.processor || {}
+        this.flag = typeof option.autorun === 'bool' ? option.autorun : false
+        this.isRunning = false
+        this.handler = function () {
+            if (this.flag) {
+                for (key in this.processor) {
+                    setTimeout(this.processor[key], 0)
+                }
+                setTimeout(this.handler.bind(this), this.timer)
             }
-            setTimeout(this.handler.bind(this), this.timer)
+        }
+        option.autorun && this.go()
+    }
+
+    PublicInterval.prototype.go = function () {
+        this.flag = true
+        !this.isRunning && this.handler()
+    }
+
+    PublicInterval.prototype.stop = function () {
+        this.isRunning = false
+        this.flag = false
+    }
+
+    PublicInterval.prototype.removeProcessor = function (key) {
+        delete(this.processor[key])
+    }
+
+    PublicInterval.prototype.setProcessor = function (key, fn) {
+        if (typeof fn === 'function') {
+            this.processor[key] = fn
+        } else {
+            throw('This processor is not a function.')
         }
     }
-    option.autorun && this.go()
-}
 
-PublicInterval.prototype.go = function () {
-    this.flag = true
-    !this.isRunning && this.handler()
-}
-
-PublicInterval.prototype.stop = function () {
-    this.isRunning = false
-    this.flag = false
-}
-
-PublicInterval.prototype.removeProcessor = function (key) {
-    delete(this.processor[key])
-}
-
-PublicInterval.prototype.setProcessor = function (key, fn) {
-    if (typeof fn === 'function') {
-        this.processor[key] = fn
-    } else {
-        throw('This processor is not a function.')
+    PublicInterval.prototype.setTimer = function (time) {
+        this.timer = +time
     }
-}
 
-PublicInterval.prototype.setTimer = function (time) {
-    this.timer = +time
-}
-
-module.exports = PublicInterval
+    return PublicInterval
+})
--- readme.md
+++ readme.md
@@ -19,24 +19,32 @@ pi.go()
 
 ## API
 ### pi.go
-- Start this interval handler.
-- param {object} obj
-    - property {number} timer Interval time of this handler.
-    - property {object} processor Initial processors of this handler.
-    - property {boolean} autorun Whether run this handler when initialized.
+- description
+    - Start this interval handler.
+- param
+    - {object} obj
+        - property {number} timer Interval time of this handler.
+        - property {object} processor Initial processors of this handler.
+        - property {boolean} autorun Whether run this handler when initialized.
 
 ### pi.stop
 - Stop this interval handler.
 
 ### pi.setProcessor
-- Set a new processor in this interval handler.
-- param {string} name The name of this processor. It's a unique value.
-- param {function} fn The processor.
+- description
+    - Set a new processor in this interval handler.
+- param
+    - name {string} The name of this processor. It's a unique value.
+    - fn {function} The processor.
 
 ### pi.removeProcessor
-- Remove a processor in this interval by name.
-- param {string} name The name of a processor which works in this interval handler.
+- description
+    - Remove a processor in this interval by name.
+- param
+    - name {string} The name of a processor which works in this interval handler.
 
 ### pi.setTimer
-- Set a interval time for this interval handler.
-- param {number} time The interval time of this interval handler.
+- description
+    - Set interval time for this interval handler.
+- param
+    - time {number} The interval time of this interval handler.
