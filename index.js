0b3fe3fdf3c7279d5389e67b0aa55a40da76bb60
Dafrok <88609546@qq.com>
2016-02-04 00:10:36 +0800

    polyfill setImmediate

--- index.js
+++ index.js
@@ -7,16 +7,26 @@
         global.PublicInterval = factory();
     }
 })(this, function () {
+    // setImmediate polyfill
+    typeof setImmediate !== 'function' && (setImmediate = function (fn) {
+        setTimeout(fn, 0)
+    })
+
+    // class PublicInterval
     function PublicInterval (option) {
         option = option || {}
         this.timer = option.timer || 16.7
         this.processor = option.processor || {}
+        this.anonymousProcessor = []
         this.flag = typeof option.autorun === 'bool' ? option.autorun : false
         this.isRunning = false
         this.handler = function () {
             if (this.flag) {
                 for (key in this.processor) {
                     setImmediate(this.processor[key])
+                }
+                for (var i = 0; i < this.anonymousProcessor.length; i++) {
+                    setImmediate(this.anonymousProcessor[i])
                 }
                 setTimeout(this.handler.bind(this), this.timer)
             }
@@ -26,12 +36,15 @@
 
     PublicInterval.prototype.go = function () {
         this.flag = true
-        !this.isRunning && this.handler()
+        !this.isRunning && (function (that) {
+            that.isRunning = true
+            that.handler()
+        })(this)
     }
 
     PublicInterval.prototype.stop = function () {
-        this.isRunning = false
         this.flag = false
+        this.isRunning = false
     }
 
     PublicInterval.prototype.removeProcessor = function (key) {
@@ -41,6 +54,9 @@
     PublicInterval.prototype.setProcessor = function (key, fn) {
         if (typeof fn === 'function') {
             this.processor[key] = fn
+        } else if (typeof key === 'function') {
+            fn = arguments[0]
+            this.anonymousProcessor.push(fn)
         } else {
             throw('This processor is not a function.')
         }
@@ -48,6 +64,12 @@
 
     PublicInterval.prototype.setTimer = function (time) {
         this.timer = +time
+    }
+
+    PublicInterval.prototype.dispose = function (time) {
+        this.processor = {}
+        this.anonymousProcessor = []
+        this.stop()
     }
 
     return PublicInterval
